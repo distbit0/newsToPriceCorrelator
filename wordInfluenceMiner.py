3174186d35212258590dbf76c8bf64361d58cbf7
Alex Pimania <randomiseme33@gmail.com>
2017-04-29 13:42:01 +1000

    Update wordInfluenceMiner.py

--- wordInfluenceMiner.py
+++ wordInfluenceMiner.py
@@ -14,18 +14,18 @@ def removeText(text, term="https?://[^\s]+"):
   
   
 def chunks(listToCut, maxLength):
-   for i in range(0, len(listToCut), maxLength):
+    for i in range(0, len(listToCut), maxLength):
       yield listToCut[i:i+maxLength]
 
 
 def initTwitterApi():
    import tweepy
-   twitterKeys = open("twitterKeysMiner.txt").read().strip().split() 
+   twitterKeys = open("twitterKeysPredictor.txt").read().strip().split() 
    auth = tweepy.OAuthHandler(twitterKeys[0], twitterKeys[1])
    auth.set_access_token(twitterKeys[2], twitterKeys[3])
    return tweepy.API(auth, wait_on_rate_limit_notify=True, wait_on_rate_limit=True)
-
-
+   
+   
 def getTwitterPosts(coinNames, period):
    import tweepy
    import time
@@ -35,10 +35,9 @@ def getTwitterPosts(coinNames, period):
    
    coinNames = [key for key in coinNames.keys()]
    api = initTwitterApi()
-   sinceDate = datetime.fromtimestamp(time.time() - period * 2).strftime('%Y-%m-%d')
-   untilDate = datetime.fromtimestamp(time.time() - period).strftime('%Y-%m-%d')
+   sinceDate = datetime.fromtimestamp(time.time() - period).strftime('%Y-%m-%d')
    for chunk in chunks(coinNames, 10):
-      for tweet in tweepy.Cursor(api.search, q=" OR ".join(chunk), tweet_mode="extended", since=sinceDate, until=untilDate, lang="en").items(1000):
+      for tweet in tweepy.Cursor(api.search, q=" OR ".join(chunk), tweet_mode="extended", since=sinceDate, lang="en").items(1000):
          tweetText = removeText(tweet._json["full_text"]).lower().strip()
          translator = str.maketrans('', '', string.punctuation)
          tweetText = tweetText.translate(translator)
@@ -58,6 +57,9 @@ def getCoinNames():
       if not coinList[coin]["name"].lower() in ignoredCoins and coin in coinMarketList:
          coinNames[coinList[coin]["name"].lower()] = "BTC_" + coin.upper()
    return coinNames
+
+
+
 
 
 def amalgamatePosts(coinNames, period):
@@ -90,86 +92,46 @@ def categorizePosts(posts, coinNames):
    return categorizedPosts
  
 
-def getWordFrequency(catgeorizedPosts):
+def getWordFrequency(categorizedPosts):
    import string
    from nltk import FreqDist
    wordFrequencies = {}
-   for coin in catgeorizedPosts:
+   for coin in categorizedPosts:
       wordFrequencies[coin] = {}
-      wordOccurences = FreqDist(catgeorizedPosts[coin].split()).most_common()
-      bigrams = [b[0] + " " + b[1] for b in zip(catgeorizedPosts[coin].split(" ")[:-1], catgeorizedPosts[coin].split(" ")[1:])]
-      wordOccurences.extend(FreqDist(bigrams).most_common())
-      totalWordCount = len(catgeorizedPosts[coin].split())
+      bigrams = [b[0] + " " + b[1] for b in zip(categorizedPosts[coin].split(" ")[:-1], categorizedPosts[coin].split(" ")[1:])]
+      allWords = categorizedPosts[coin].split()
+      allWords.extend(bigrams)
+      wordOccurences = FreqDist(totalWords).most_common()
+      totalWordCount = len(wordOccurences)
       for word in wordOccurences:
          wordCountRatio = (word[1] / totalWordCount) * 100
          wordFrequencies[coin][word[0]] = wordCountRatio
    return wordFrequencies
-
-
-def getPriceMovement(coinNames, period):
-   import time
-   from poloniex import Poloniex
-   polo = Poloniex()
-   coinPriceChanges = {}
-   for coin in coinNames:
-      startTime = time.time() - period
-      coinWtdAvg = float(polo.returnChartData(coinNames[coin], start=startTime)[0]["weightedAverage"])
-      lastCoinWtdAvg = float(polo.returnChartData(coinNames[coin], start=startTime - period, end=startTime)[0]["weightedAverage"])
-      changePercent = ((coinWtdAvg / lastCoinWtdAvg) * 100) - 100
-      coinPriceChanges[coin] = changePercent  
-   return coinPriceChanges
-
-
-def getWordsInfluence(coinPriceChanges, wordFrequencies):
-   wordInfluences = {}
-   for coin in wordFrequencies:
-      coinPriceChange = coinPriceChanges[coin]
-      for word in wordFrequencies[coin].keys():
-         if not word in wordInfluences.keys():
-            wordInfluences[word] = [0, 0]
-         incrementCount = wordInfluences[word][1]
-         totalInfluence = wordInfluences[word][0]
-         wordInfluence = wordFrequencies[coin][word] * coinPriceChange
-         wordInfluences[word] = [totalInfluence + wordInfluence, incrementCount + 1]
-   return wordInfluences
-
-
-def updateFile(wordInfluences):
+   
+   
+def getCoinScores(wordFrequencies):
    import json
-   wordInfluencesFile = json.loads(open("wordInfluences.json").read())
-   for word in wordInfluences:
-      if not word in wordInfluencesFile:
-         wordInfluencesFile[word] = [0, 0]
-      incrementCount = wordInfluencesFile[word][1]
-      totalInfluence = wordInfluencesFile[word][0]
-      wordInfluencesFile[word] = [totalInfluence + wordInfluences[word][0], incrementCount + wordInfluences[word][1]]
-   with open("wordInfluences.json", "w") as wordInfluencesFileObj:
-      wordInfluencesFileObj.write(json.dumps(wordInfluencesFile, indent=2))
-
-
-def getDelayTime():
-   import time
-   secondsPerDay = 60*60*24
-   currentTime = time.time()
-   secondsSinceMidnight = currentTime % secondsPerDay
-   secondsUntilMidnight = secondsPerDay - secondsSinceMidnight
-   return secondsUntilMidnight
+   coinScores = {}
+   wordInfluences = json.loads(open("wordInfluences.json").read())
+   avgWordScore = sum([value[0] / value[1] for value in wordInfluences.values()]) / float(len(wordInfluences))
    
+   for coin in wordFrequencies:
+      coinScore = 0
+      validWords = 0
+      for word in wordFrequencies[coin]:
+         if word in wordInfluences:
+            coinScore += wordInfluences[word][0] / wordInfluences[word][1]
+            validWords += 1
+      coinScore = coinScore/validWords
+      coinScores[coin] = coinScore
+   return [avgWordScore, coinScores]
    
-import time
-import sys
-while True:
-   time.sleep(getDelayTime())
-   while True:
-      try:
-         coinNames = getCoinNames()
-         posts = amalgamatePosts(coinNames, period)
-         categorizedPosts = categorizePosts(posts, coinNames)
-         wordFrequencies = getWordFrequency(categorizedPosts)
-         coinPriceChanges = getPriceMovement(coinNames, period)
-         wordInfluences = getWordsInfluence(coinPriceChanges, wordFrequencies)
-         updateFile(wordInfluences)
-         break
-      except:
-         print("Exception occured: \n" + sys.exc_info()[1])
-         continue
+def saveCoinScores(coinScores):
+   print(coinScores)
+   
+coinNames = getCoinNames()
+posts = amalgamatePosts(coinNames, period)
+categorizedPosts = categorizePosts(posts, coinNames)
+wordFrequencies = getWordFrequency(categorizedPosts)
+coinScores = getCoinScores(wordFrequencies)
+saveCoinScores(coinScores) #[1]
