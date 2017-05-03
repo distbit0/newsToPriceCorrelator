54ec6e0b9ba5323af84b8a997e3251d35e74bddc
Alex Pimania <randomiseme33@gmail.com>
2017-05-03 18:14:06 +1000

    Update wordInfluenceMiner.py

--- wordInfluenceMiner.py
+++ wordInfluenceMiner.py
@@ -12,10 +12,12 @@ def removeText(text, term="https?://[^\s]+"):
       else: break
    return text
   
-  
+   
 def chunks(listToCut, maxLength):
+   chunkList = []
    for i in range(0, len(listToCut), maxLength):
-      yield listToCut[i:i+maxLength]
+      chunkList.append(listToCut[i:i+maxLength])
+   return chunkList
 
 
 def initTwitterApi(config):
