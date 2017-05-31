541f77d2226b0c1d9aaffbf0afbeefcdaa4a8312
Alex Pimania <randomiseme33@gmail.com>
2017-06-01 06:59:59 +1000

    Create coinScoresToExcel.py

---
+++ coinScoresToExcel.py
@@ -0,0 +1,16 @@
+	import csv
+	import json
+	keys = []
+	historicalCoinScores = json.loads(open("/home/ap/newsToPriceCorrelator-master/historicalCoinScores.json").read())
+	timedCoinScores = []
+	for coinScores in historicalCoinScores:
+		coinScores["coinScores"]["time"] = coinScores["time"][1]
+		timedCoinScores.append(coinScores)
+	toCSV = [coinScores["coinScores"] for coinScores in timedCoinScores]
+	for coinScores in toCSV:
+		keys.extend(list(coinScores.keys()))
+	keys = list(set(keys))
+	with open('people.csv', 'w') as output_file:
+	    dict_writer = csv.DictWriter(output_file, keys)
+	    dict_writer.writeheader()
+	    dict_writer.writerows(toCSV)
