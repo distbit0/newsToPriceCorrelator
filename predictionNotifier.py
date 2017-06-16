3b60b6765b4c83a942eb4595cfc6b19d959580d9
Alex Pimania <randomiseme33@gmail.com>
2017-06-16 13:23:52 +1000

    Update predictionNotifier.py

--- predictionNotifier.py
+++ predictionNotifier.py
@@ -1,11 +1,22 @@
+def getChartData():
+	import csv
+	reader = csv.DictReader(open('coinScoresExcel.csv'))
+	result = {}
+	for row in reader:
+		key = row.pop('time')
+		result[key] = row
+	return result
+		
+		
 def generateGraph():
+	getChartData
 	import matplotlib.pyplot as plt
-	result = {'apr': 13, 'jun': 13, 'jul': 13, 'aug': 13}
-	plt.plot(list(result.values()), label="1")
+	chartData = getChartData()
+	plt.plot(list(chartData.values()), label="1")
 	plt.ylabel('Number')
-	plt.xlabel('Time (Months)')
-	plt.title('Number per month')
-	plt.xticks(range(len(result)), result.keys())
+	plt.xlabel('Time (Days)')
+	plt.title('Cryptocurrency predictions')
+	plt.xticks(range(len(chartData)), chartData.keys())
 	plt.legend()
 	plt.savefig("ff.png")
    
@@ -13,18 +24,7 @@ def generateCoinWordsText():
    pass
    
 def sendEmails():
-   import smtplib
-   smtpobj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-   smtpobj.login("snazzyemailsender@gmail.com", "wsxcdeWSXCDE99!wsxcdeWSXCDE99!")
-   smtpobj.sendmail("snazzyemailsender@gmail.com", "brainpasf@gmail", "Subject:jdjdjd\n\n\n\n fhrfrrgruyfgrydjssjgd")
-
-
-import csv
-reader = csv.DictReader(open('coinScoresExcel.csv'))
-
-result = {}
-for row in reader:
-    key = row.pop('time')
-    result[key] = row
-    
-print(result)
+	import smtplib
+	smtpobj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+	smtpobj.login("snazzyemailsender@gmail.com", "wsxcdeWSXCDE99!wsxcdeWSXCDE99!")
+	smtpobj.sendmail("snazzyemailsender@gmail.com", "brainpasf@gmail", "Subject:jdjdjd\n\n\n\n fhrfrrgruyfgrydjssjgd")
