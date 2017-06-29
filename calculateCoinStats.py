e8608a07382f35c935e3950b50473ea42e711aaa
Alex Pimania <randomiseme33@gmail.com>
2017-06-29 21:08:23 +1000

    Update calculateCoinStats.py

--- calculateCoinStats.py
+++ calculateCoinStats.py
@@ -1,7 +1,9 @@
 import wordInfluencePredictor
 import time
-coinScores = getCoinScores()
+coinScores, coinWords = wordInfluencePredictor.getCoinScores()
 currentTime = time.strftime("%Z - %d/%m/%Y, %H:%M:%S", time.localtime(time.time()))
 print("Current time: " + currentTime)
+for coin in coinWords:
+   print(coin + " goodWords: " + str(coinWords[coin]["good"]) + " badWords: " + str(coinWords[coin]["bad"]) + "\n\n")
 for coin in sorted(coinScores.items(), key=lambda x: x[1]):
-  print(coin[0] + " " + str(coin[1]))
+   print(coin[0] + " " + str(coin[1]))
